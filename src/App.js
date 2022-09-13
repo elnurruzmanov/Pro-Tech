207f9fa85139e163121cda293e033ceb41d9ead2
Elnur Ruzmanov <80697079+elnurruzmanov@users.noreply.github.com>
2022-09-13 17:26:08 +0300

    I deleted the Routers from the App

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import React from "react";
 //Routers
 import { Routes, Route } from "react-router-dom";
 
-
 //components
 import Home from "./components/Home/Home";
 import About from "./components/About/About";
@@ -16,27 +15,19 @@ import Partners from "./components/Partners/Partners";
 function App() {
   return (
     <div className="App">
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
-      <Routes>
-        <Route path="/" element={<About />} />
-      </Routes>
-      <Routes>
-        <Route path="/" element={<Repair />} />
-      </Routes>
-      <Routes>
-        <Route path="/" element={<Services />} />
-      </Routes>
-      <Routes>
-        <Route path="/" element={<Clients />} />
-      </Routes>
-      <Routes>
-        <Route path="/" element={<Works />} />
-      </Routes>
-      <Routes>
-        <Route path="/" element={<Partners />} />
-      </Routes>
+      <Home />
+
+      <About />
+
+      <Repair />
+
+      <Services />
+
+      <Clients />
+
+      <Works />
+
+      <Partners />
     </div>
   );
 }
